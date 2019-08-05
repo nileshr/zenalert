c03763dfb1245d13681885a23748f3fb432a5d8e
Nilesh Rathi <nileshrathi01@gmail.com>
2019-08-05 20:44:38 +0530

    added dependency alert monitoring!

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This can become overwhelming for large projects, and so we need to use [Zenhub's
 
 ## How?
 
-- Download the latest build from [releases](https://github.com/nileshr/zenalert/releases/latest). 
+- Download the latest build from [releases](https://github.com/nileshr/zenalert/releases/latest).
 - Alternatively, if you want to build it yourself, clone this repo and install the dependency. (Run `npm install` or `yarn` in project root) and create the deployment package by zipping the project folder (On MacOS, Run `zip -r build.zip *` in project root).
 - Create a Lambda function in AWS with basic lambda execution policy and upload the build.zip.
 - Set [API Gateway as the trigger](https://docs.aws.amazon.com/lambda/latest/dg/invoking-lambda-function.html#supported-event-source-api-gateway) and provide a name to the API. Configure authentication as open, since ZenHub can only trigger open webhooks. Copy the _execution url_ provided by API Gateway after saving the changes.
@@ -20,3 +20,4 @@ This can become overwhelming for large projects, and so we need to use [Zenhub's
 - To get issue transfer notifications for only some pipelines, you can _optionally_ set `PIPELINES` environment variable with the comma-separated names of the pipelines in your project. Note: This value is ignored if `issue_transfer` is not set in `EVENT_TYPES`.
 - If you want to tag certain people on Slack when there's activity on a particular pipeline, you can optionally set the `TAG_MAP` environment variable which is a JSON map of pipeline names to Slack names (multiple names are separated by white-space). For instance, if you want to tag `@nr` and `@test-team` when there's movement in `Issues` pipeline, you should create a `TAG_MAP` env var with the value `{"Issues": "@nr @test-team"}`
 - _Finally,_ on [ZenHub Dashboard](https://app.zenhub.com/dashboard), on the Integrations tab create a new custom integration. Select the repo to connect, and provide the Lambda _execution url_ as the webhook url.
+- In addition to this, if you would like monitor dependencies and send alerts when they get cleared, set `DEPENDENCY_PIPELINES` env variable similar to `PIPELINES`. You'll also need to set the `DEPENDENCY_REPO_ID` in which dependencies are being marked and the ZenHub API token `ZH_API_TOKEN` to fetch these dependencies. (Refer [ZenHub's API](https://github.com/ZenHubIO/API) for more on these 2 variables.)
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const axios = require("axios");
 const querystring = require("querystring");
 
-const { SLACK_WEBHOOK_URL, EVENT_TYPES, PIPELINES, TAG_MAP } = process.env;
+const {
+  SLACK_WEBHOOK_URL,
+  EVENT_TYPES,
+  PIPELINES,
+  TAG_MAP,
+  DEPENDENCY_REPO_ID,
+  DEPENDENCY_PIPELINES,
+  ZH_API_TOKEN
+} = process.env;
 
 const EVENT_MAP = {
   ISSUE_TRANSFER: "issue_transfer",
@@ -14,14 +22,18 @@ const tagMap = JSON.parse(TAG_MAP);
 
 const buildIssue = data => `<${data.github_url}|#${data.issue_number} ${data.issue_title}>`;
 const moveIssue = data =>
-  `${data.user_name} moved ${buildIssue(data)} to ${data.to_pipeline_name} ${tagMap[data.to_pipeline_name] || ""}`.trim();
+  `${data.user_name} moved ${buildIssue(data)} to ${data.to_pipeline_name} ${tagMap[data.to_pipeline_name] ||
+    ""}`.trim();
 
-exports.handler = (event, context, callback) => {
+const getArrayFromString = str => (str || "").split(",").map(t => t.trim());
+
+exports.handler = (event, _, callback) => {
   if (event.body) {
     const data = querystring.parse(event.body);
-    const eventTypes = (EVENT_TYPES || "").split(",").map(t => t.trim());
-    const pipelines = (PIPELINES || "").split(",").map(p => p.trim());
-    let text;
+    const eventTypes = getArrayFromString(EVENT_TYPES);
+    const pipelines = getArrayFromString(PIPELINES);
+    const dependencyPipelines = getArrayFromString(DEPENDENCY_PIPELINES);
+    let text, checkDependencies;
     if (eventTypes.includes(data.type)) {
       switch (data.type) {
         case EVENT_MAP.ISSUE_TRANSFER:
@@ -31,6 +43,10 @@ exports.handler = (event, context, callback) => {
             } // No else - Ignore if not in filtered pipeline!
           } else {
             text = moveIssue(data);
+          }
+          // There's surely a cleaner way; but this seems to be easiest for now :p
+          if (dependencyPipelines.length > 0 && dependencyPipelines.includes(data.to_pipeline_name)) {
+            checkDependencies = true;
           }
           break;
         case EVENT_MAP.ESTIMATE_SET:
@@ -47,12 +63,29 @@ exports.handler = (event, context, callback) => {
       }
       if (text) {
         axios.post(SLACK_WEBHOOK_URL, { text, link_names: 1 }).catch(console.log);
-        callback(null, {
-          statusCode: "200",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({})
-        });
-      } else callback("Error! Unknown Event Type!");
+      }
+      if (checkDependencies) {
+        axios
+          .get(`https://api.zenhub.io/p1/repositories/${DEPENDENCY_REPO_ID}/dependencies?access_token=${ZH_API_TOKEN}`)
+          .then(response => {
+            // Not doing a strict equality check since types are different
+            const blockerIssue = response.data.dependencies.find(
+              issue => issue.blocking.issue_number == data.issue_number
+            );
+            if (blockerIssue) {
+              const blockedIssueUrl = `${data.github_url.replace(/\d+$/, "")}${blockerIssue.blocked.issue_number}`;
+              text = `The issue ${buildIssue(data)} is now ${
+                data.to_pipeline_name
+              } 🎉🎉 It was a blocker for ${blockedIssueUrl} ${tagMap["Dependencies"] || ""}`;
+              axios.post(SLACK_WEBHOOK_URL, { text, link_names: 1 }).catch(console.log);
+            }
+          });
+      }
+      callback(null, {
+        statusCode: "200",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({})
+      });
     }
   } else {
     callback("Error! No event body!");
